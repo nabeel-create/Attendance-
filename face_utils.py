b7c1b1179c30eb9f1a75f353eedf64b791d26188
nabeel-create <nabeelrafique101@gmail.com>
2025-11-24 16:12:23 +0500

    Update face_utils.py

--- face_utils.py
+++ face_utils.py
@@ -1,7 +1,5 @@
 import face_recognition
-import cv2
 import os
-import numpy as np
 
 def encode_faces(folder='students'):
     encodings = {}
@@ -14,10 +12,15 @@ def encode_faces(folder='students'):
                 encodings[filename.split('.')[0]] = face_encoding[0]
     return encodings
 
-def recognize_face(known_encodings, frame):
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    faces = face_recognition.face_locations(rgb_frame)
-    encodings = face_recognition.face_encodings(rgb_frame, faces)
+def recognize_face(known_encodings, uploaded_image):
+    import numpy as np
+    import cv2
+    file_bytes = np.asarray(bytearray(uploaded_image.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, 1)
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    faces = face_recognition.face_locations(rgb_img)
+    encodings = face_recognition.face_encodings(rgb_img, faces)
 
     for encoding in encodings:
         for roll_number, known_encoding in known_encodings.items():
